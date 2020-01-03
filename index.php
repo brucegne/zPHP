610843c43444d49129c96de9afb21c936632f569
Bruce Gordon <brucegne@gmail.com>
2020-01-03 17:15:36 -0500

    json

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 	echo "PHP is now working<br /><br />";
   $json = file_get_contents("https://zpie.now.sh/json");
   $arr = json_decode($json);
-  var_dump($arr->records);
   $recs = $arr->records;
   for($i=0;$i<5;$i++){
     $rec = $recs[$i];
@@ -11,9 +10,5 @@
     echo("<br />");
   }
   echo "<br /><br />";
-  foreach($arr as $key=>$value){
-      echo $key . "=>" . $value . "<br>";
-  }
-  echo "<hr>";  
 ?>
 <p><a href="./aircrud.php">Test AirTable</a></p>
