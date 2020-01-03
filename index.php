828ec8f977cca121552428febf95880322f2005f
Bruce Gordon <brucegne@gmail.com>
2020-01-03 14:54:25 -0500

    arr

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
   $json = file_get_contents("https://zpie.now.sh/json");
   $arr = json_decode($json);
   var_dump($arr->records);
+  $recs = $arr->records;
+  for($i=0;$i<5;$i++){
+    echo($recs[i].name);
+    echo("<br />");
+  }
   echo "<br /><br />";
   foreach($arr as $key=>$value){
       echo $key . "=>" . $value . "<br>";
