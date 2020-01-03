2ac05c5901c0730c1a32f2f31c5f7b309ce189e6
Bruce Gordon <brucegne@gmail.com>
2020-01-03 12:44:16 -0500

    index

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	echo "PHP is now working";
   $json = file_get_contents("https://zpie.now.sh/json");
   $arr = json_decode($json);
+  var_dump($arr);
   foreach($arr as $key=>$value){
       echo $key . "=>" . $value . "<br>";
   }
