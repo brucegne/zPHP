6cd67b03cede556ccc4b9093d165c7ed3431f7b8
Bruce Gordon <brucegne@gmail.com>
2020-01-04 04:46:52 -0500

    post

--- index.php
+++ index.php
@@ -10,6 +10,34 @@
     echo("<br />");
   }
   echo "<br /><br />";
+
+$url = 'https://zpie.now.sh/adddata';
+$data = array('created' => '12345678', 'name' => 'Good Old Budders', 'age' => '13', 'married' => 'Just mommy Kellie');
+$options = array(
+        'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+    )
+);
+
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+var_dump($result);
+
+/*
+$url = 'http://www.someurl.com';
+$myvars = 'myvar1=' . $myvar1 . '&myvar2=' . $myvar2;
+
+$ch = curl_init( $url );
+curl_setopt( $ch, CURLOPT_POST, 1);
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
+curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt( $ch, CURLOPT_HEADER, 0);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+$response = curl_exec( $ch );
+*/
 ?>
 <p><a href="./aircrud.php">Test AirTable</a></p>
 
