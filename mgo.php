51d9a3a7ba4721ca312bba0cac46c49b190486f9
Bruce Gordon <brucegne@gmail.com>
2020-01-09 21:23:32 -0500

    redo

--- mgo.php
+++ mgo.php
@@ -1,13 +1,9 @@
 <?php
 $connection = new MongoClient('mongodb://brucegne:p2shiver@ds043368.mongolab.com:43368/demo');
 $db = $connection->demo;
-
-// select a collection:
 $collection = $db->contacts;
 
 $cursor = $collection->find()->sort(array('lname' => 1));
-// $collection->insert(array("fname" => "George","lname"=>"Doggy"));
-
 foreach ($cursor as $rec ) {
   echo $rec['_id'].'   '.$rec['fname'].'  '.$rec['lname'].'<br />';
 }
